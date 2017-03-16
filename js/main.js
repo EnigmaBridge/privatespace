336c10aaa8f2d94fe7acbb5bc2c1d06b50f8a2d1
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-16 18:14:57 +0100

    Live stats fixes - decimals for MB

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function loadStatsData(onLoaded, onFail){
 function formatBytes(bytes, decimals) {
     if(bytes == 0) return '0 Bytes';
     var k = 1000,
-        dm = decimals + 1 || 3,
+        dm = decimals + 1 || 0,
         sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
         i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
